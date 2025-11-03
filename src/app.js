1f22c2500c04b41d671bb59bc0d2411383d9e1ae
Andrés Felipe Rangeles <andresrangelesparza@gmail.com>
2025-11-03 14:39:57 -0500

    first commit - web server

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import express from "express";
+const app = express();
+app.set("port", 5000)
+export default app;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import app from "./app.js";
+const main = () => {
+    app.listen(app.get("port"));
+    console.log(`The company´s super web server is running on port ${app.get("port")}`);
+
+    
+}
+main();
+
+
